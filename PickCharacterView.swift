33e0f94e13cb5d086338f7676005812045d8cdfb
Bartosz Odrzywołek <Bart@Bartoszs-MacBook-Pro.local>
2016-04-26 17:51:52 +0200

    Add another viewcontroller

---
+++ PickCharacterView.swift
@@ -0,0 +1,18 @@
+//
+//  PickCharacterView.swift
+//  little-monster
+//
+//  Created by Bartosz Odrzywołek on 26/04/16.
+//  Copyright © 2016 Bartosz. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class PickCharacterView: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        
+    }
+}
